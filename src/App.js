c32f4fa0ae310c5a9a1f85beae1fc1b687feb174
hafizakbar19 <hafizakbar19@gmail.com>
2023-10-11 19:01:41 +0500

    React General Practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Data} from './Data';
+import ClassComp from './ClassComp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Data name='Akbar' age='35' />
+      <ClassComp value='this is value of class based function from prop'/>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ function Data({name,age}) {
+  return (
+    <div>
+        <h1>{name}</h1>
+        <h2>{age}</h2>
+    </div>
+  )
+}
+export {Data};
