7dc27eb178366a7e1bd65ce9a07caa5511f61a6f
hafizakbar19 <hafizakbar19@gmail.com>
2023-10-26 18:21:53 +0500

    list keys

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -16,11 +16,21 @@ function ListRender() {
             name: 'Haniya',
             age: 5,
             class: 1
+        },
+        {
+            name: 'Taha',
+            age: 12,
+            class: 6
+        },
+        {
+            name: 'Sadia',
+            age: 12,
+            class: 6
         }
     ];
   return (
     <div>
-    { students.map(std => <h1>I am {std.name}, My age is {std.age} and I study in class {std.class}</h1>) }
+    { students.map((std,index) => <h1 key={index}>I am {std.name}, My age is {std.age} and I study in class {std.class}</h1>) }
     </div>
   )
 }
